5617c6c5413cd40d2b043568b541f570f9068c28
Mariana Martins <mrrvm@hotmail.com>
2023-11-09 21:13:49 +0100

    tostring

--- src/app/_components/dateTag.tsx
+++ src/app/_components/dateTag.tsx
@@ -1,10 +1,14 @@
 export const DateTag = ({ date }: { date: string }) => {
     const newDate = new Date(date);
     return (
-        <span className="font-bold">
-            {`${newDate.toLocaleString('default', {
-                month: 'long'
-            })} ${newDate.getFullYear()}`}
-        </span>
+        <>
+            {newDate && (
+                <span className="font-bold">
+                    {`${newDate.toLocaleString('default', {
+                        month: 'long'
+                    })} ${newDate.getFullYear()}`}
+                </span>
+            )}
+        </>
     );
 };
