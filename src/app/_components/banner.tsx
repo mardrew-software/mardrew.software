ff11ab31dfca9e4ce1de92a6505be459d3a88d81
Mariana Martins <mrrvm@hotmail.com>
2023-11-13 14:25:39 +0100

    limit image

--- src/app/_components/banner.tsx
+++ src/app/_components/banner.tsx
@@ -7,7 +7,7 @@ export const Banner = () => {
     return (
         <div className="flex flex-col lg:flex-row items-center gap-8 lg:gap-16 py-8 lg:py-16 px-8 lg:px-16 lg:px-16 bg-[#ffe280]">
             <Image
-                className="w-[80%]"
+                className="w-[80%] max-w-[740px]"
                 alt="mardrew.software"
                 src={'/logo_pink_rebranded.png'}
                 width={500}
