41a373b00fa92185564bafccf69c538292a193d2
Mariana Martins <mrrvm@hotmail.com>
2023-11-14 23:39:38 +0100

    order by

--- src/app/_api/getCredits.ts
+++ src/app/_api/getCredits.ts
@@ -8,7 +8,7 @@ export async function getCredits() {
         body: JSON.stringify({
             query: `        
             query Credits {
-              credits {
+              credits(orderBy: startDate_DESC) {
                 description {
                   html
                   text
